578bf0a649aa6e6447144ff3d1cbb489830d9555
Matan Romi <43634754+matanrom@users.noreply.github.com>
2022-12-17 00:28:17 +0200

    pass test tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = (fahrenheit - 32) * 0.5556
+  if(converted % 1 != 0) converted =  converted.toFixed(1)
+  return +converted
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let converted = celsius * 1.8 + 32;
+  if(converted % 1 != 0) converted = converted.toFixed(1);
+  return +converted
 };
 
 // Do not edit below this line
