ad8513152162e41cc9e25677e51b3b2672d561b6
Matan Romi <43634754+matanrom@users.noreply.github.com>
2022-12-16 23:33:23 +0200

    pass test sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+  let sum = 0;
+  let small;
+  let large;
 
+  if(from < 0 || to < 0 || typeof(from) != 'number' || typeof(to) != 'number') return 'ERROR';
+  
+  if(from < to){
+    small = from;
+    large = to;
+  }else{
+    small = to;
+    large = from;
+  }
+
+  for(let i = small; i <= large; i++) {
+    sum += i;
+  }
+  return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
